e06bf5d57953e7247b849d970d664b2ea4f7b13e
jwaes <jaco.waes@gmail.com>
2023-01-25 10:37:28 +0000

    langfix

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'stock',
-    'version': '0.15',
+    'version': '0.16',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase_stock','mrp_subcontracting_dropshipping'],
