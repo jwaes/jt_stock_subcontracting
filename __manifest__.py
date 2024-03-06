0f10849bbc24be6a65789757a0b5cd913ead7820
jwaes <jaco.waes@gmail.com>
2024-03-06 10:35:46 +0000

    attrs

--- __manifest__.py
+++ __manifest__.py
@@ -14,7 +14,7 @@
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'stock',
-    'version': '0.18',
+    'version': '2.0',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase_stock','mrp_subcontracting_dropshipping'],
